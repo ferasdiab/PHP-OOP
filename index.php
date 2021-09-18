c45bafcf11a08b0f473336a5340ad9f4e2aad88e
ferasdiab <dfiras96@gmail.com>
2021-09-18 23:02:33 +0300

    add file

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php 
+
+echo "hello";
+echo "<br>";
+
+//// testing  
+
+
+
+// class User{
+ 
+//     private $userName;
+//     public $email;
+//     private $password;
+ 
+//    function __construct($userName, $email, $pass){
+//         $this->userName= $userName;
+//         $this->email= $email;
+//         $this->password = $pass;
+//     }
+//     function getUserName () {
+//         return $this->userName;
+//     }
+// }
+
+// $userOne = new User("Firas", "Firas@mail.com" , "123");
+
+// echo($userOne->getUserName()) . '<br>' ;
+
+
+// class Admin extends User {
+//     public $role = "admin";
+// }
+
+// $adminOne = new Admin("omar", "omar@mail.com" , "123");
+
+// print_r($adminOne->getUserName());
+
+// print_r($adminOne->userName);
+
+
+
+
+
+?>
+
+
